c977d2d90e6b0d566992b369e2cae89ab9a2dcd9
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-26 17:45:14 +0600

    user schema and model created

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
 import connectDB from './config/database.js';
 
 // dotenv config
@@ -11,10 +13,14 @@ const app = express();
 // MongoDB Connection
 connectDB()
 
+// Middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 
 export default app;
---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import validator from "validator";
+// Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        validate: validator.isEmail,
+    },
+    passwod: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters"],
+    },
+    location: {
+        type: String,
+        default: "N/A",
+    }
+},
+{versionKey: false},
+{ timestamps: true},
+)
+
+// Model
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
